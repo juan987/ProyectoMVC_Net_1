3e902d28991925d49b904b14e7d3243491ba6448
Alumno <Alumno@FALCON>
2018-01-15 19:28:41 +0100

    Commit 2 15 ene

---
+++ ProyectoMVC_Net_1/Controllers/InformacionController.cs
@@ -0,0 +1,26 @@
+﻿using ProyectoMVC_Net_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoMVC_Net_1.Controllers
+{
+    public class InformacionController : Controller
+    {
+        // GET: Informacion
+        public ActionResult ControladorVistas()
+        {
+            //Usamos ViewBag
+            ViewBag.Nombre = "Visual Studio .NET";
+            //Enviamos el modelo Persona
+            Persona p = new Persona();
+            p.Nombre = "Juan";
+            p.Apellidos = "Navarro";
+
+            //Hay que poner el modelo en el return, solo un modelo por cada vista
+            return View( p );
+        }
+    }
+}
---
+++ ProyectoMVC_Net_1/Models/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoMVC_Net_1.Models
+{
+    public class Persona
+    {
+        public String Nombre { get; set; }
+        public String Apellidos { get; set; }
+    }
+}
