4004fc8538a8120999ae25bdac8ca003bfce7145
Alumno <Alumno@FALCON>
2018-01-15 17:58:21 +0100

    Add project files.

---
+++ ProyectoMVC_Net_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoMVC_Net_1.MvcApplication" Language="C#" %>
---
+++ ProyectoMVC_Net_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoMVC_Net_1.Startup))]
+namespace ProyectoMVC_Net_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
